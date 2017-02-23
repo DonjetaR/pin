60646e9932350abdc568293cb55e62a9c13f0c2f
Bakhtawar <bakhtawarnoor_91@hotmail.com>
2017-02-23 14:43:04 +0100

    stuff

--- pin.py
+++ pin.py
@@ -1,26 +1,47 @@
 import networkx as nx 
 import matplotlib.pyplot as plt
+import random
 
-def duplicatin_duplication_model():
-	pass
+def partial_duplication_model(p,q,s):
+        G = nx.Graph()
+        for i in range(s):
+                G.add_node(i,name = "u")
+       # for i in range(0,10):
+        
+        node = random.choice(G.nodes())
+        v = s + 1
+        G.add_node(v)
+        
+        G.add_edge(v,node,weight = q)
+        for j in range(s):
+                for i in G.neighbors(j):
+                        if i not v:
+                                G.add_edge(i,node,weight = p)
+
+
+        nx.draw(G)
+        plt.show()
+        
+                
+	
 		
 if __name__ == '__main__':
 	f=open("4932.protein.links.v10.txt","r").readlines()
 	#f.readline()
 	#for x in f:
 	#f.split("\n")
-	G=nx.Graph()
+	#G=nx.Graph()
 	#max_score=0
 	#min_score=1000
 	
 	
-	for x in f[1:]:
-		y=x.split(" ")		
-		for node in y[:1]:
+	#for x in f[1:]:
+	#	y=x.split(" ")		
+	#	for node in y[:1]:
 			#print(node)
-			G.add_node(node)
-		if int(y[2])>=500:
-			G.add_edge(y[0], y[1], weight=int(y[2]))
+	#		G.add_node(node)
+	#	if int(y[2])>=500:
+	#		G.add_edge(y[0], y[1], weight=int(y[2]))
 		#temp=int(y[2])
 		#print(type(temp))
 		#max_score=max(max_score,temp)
@@ -32,8 +53,9 @@ if __name__ == '__main__':
 	#plt.show()
 	#plt.savefig("graph.png")
 		
-	
-	duplicatin_duplication_model()
+	p = 0.3
+	q = 0.7
+	s = 2
+	partial_duplication_model(p,q,s)
 
-print("hello")
 	
