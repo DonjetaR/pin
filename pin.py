a0b2ecda65504fdd6813cb827925ce447de21e72
DonjetaR <donjeta@live.dk>
2017-02-24 11:13:53 +0100

    blabla

--- pin.py
+++ pin.py
@@ -2,36 +2,40 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 
-def partial_duplication_model(G,p,q,s,max_score):
+def partial_duplication_model(p,q,s):
     #G = nx.Graph()
     #for i in range(s):
     #        G.add_node(i,name = "u")
     # for i in range(0,10):
-	k=G.number_of_nodes()
-	list=[]
 	
+	#list=[]
+	
+	G=nx.Graph()
 	for i in range(s):
-		#random.randint(1,k)
+		G.add_node(i)
+	print("Number of nodes before",G.number_of_nodes())
+	print("Is it connected before: ",nx.is_connected(G))
+	
+	size_of_G=500
+	
+	for v in range(s,size_of_G):
+		prob=random.uniform(0,1)
+		print("prob=",prob)
 		node = random.choice(G.nodes())
-		if node not in list:
-			v = max_score + i
+		if prob>=q:
 			G.add_node(v)
-			list.append(node)
-        
-			G.add_edge(v,node,weight = q)
-	
-        #for j in range(k):
-			for j in G.neighbors(node):
-				if not j==v:
-					G.add_edge(j,node,weight = p)
+			G.add_edge(v,node)
+		#prob=random.rand(0,1)
 		
-	
-	print(G.number_of_nodes())
-	
+		for j in G.neighbors(node):
+			if not j==v and prob<=p:			
+				G.add_edge(j,v)
+	print("Number of nodes after",G.number_of_nodes())
+	print("Is it connected after: ",nx.is_connected(G))
 	return(G)
 	
 def eksperiments(G):
-	print("drawing the graph/loglog-plot")
+	print("drawing the loglog-plot")
 	#W=nx.degree_histogram(G)
 	#plt.loglog(W)
 	#plt.show()
@@ -54,35 +58,39 @@ def eksperiments(G):
 	plt.loglog(x,y)
 	plt.show()
 	#plt.savefig("loglog-plot.png")
-	#print("Average shortest path: ")
+	print("Average shortest path: ")
 	
-	#p=nx.average_shortest_path_length(G)
-	#print(p)
+	p=nx.average_shortest_path_length(G)
+	print(p)
 	
 	print("Clustering coefficient: ")
 	print(nx.average_clustering(G))
 	
+	print("drawing the graph")
+	nx.draw(G)
+	plt.show()
+	
 	
 if __name__ == '__main__':
-	f=open("4932.protein.links.v10.txt","r").readlines()
+	#f=open("4932.protein.links.v10.txt","r").readlines()
 	#f.readline()
 	#for x in f:
 	#f.split("\n")
-	G=nx.Graph()
-	max_score=0
+	#G=nx.Graph()
+	#max_score=0
 	#min_score=1000
 	
 	
-	for x in f[1:]:
-		y=x.split(" ")		
-		for node in y[:1]:
-			if int(y[2])>=990:
-				G.add_node(node)
-		if int(y[2])>=990:
-			G.add_edge(y[0], y[1], weight=int(y[2]))
-		temp=int(y[2])
+	#for x in f[1:]:
+	#	y=x.split(" ")		
+	#	for node in y[:1]:
+	#		if int(y[2])>=990:
+	#			G.add_node(node)
+	#	if int(y[2])>=990:
+	#		G.add_edge(y[0], y[1], weight=int(y[2]))
+	#	temp=int(y[2])
 		#print(type(temp))
-		max_score=max(max_score,temp)
+	#	max_score=max(max_score,temp)
 		#min_score=min(min_score,temp)
 	#print(max_score)
 	#print(min_score)
@@ -93,13 +101,13 @@ if __name__ == '__main__':
 		
 	p = 0.3
 	q = 0.7
-	s = G.number_of_nodes()
-	print(s)
+	s = 1
+	#print(s)
 	#node = random.choice(G.nodes())
 	#print(node)
-	print("before: ",nx.is_connected(G))
-	G1=partial_duplication_model(G,p,q,s,max_score)
-	print("after: ",nx.is_connected(G))
+	#print("before: ",nx.is_connected(G))
+	G1=partial_duplication_model(p,q,s)
+	#print("after: ",nx.is_connected(G))
 	eksperiments(G1)
 
 	
